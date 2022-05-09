25179ea950cee0851198f30b527c7d7c39781774
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2022-05-09 10:17:01 +0100

    CW2 started task 1 and 2

--- seedstudents.php
+++ seedstudents.php
@@ -1,7 +1,26 @@
 <?php
-	//insert/seed 5 student records
-    
 
+	include("_includes/config.inc");
+	include("_includes/dbconnect.inc");
+	include("_includes/functions.inc");
 
+	require_once 'vendor/autoload.php';
 
+		if (isset($_SESSION['id'])) 
+		{
+			$faker = Faker\Factory::create();
+
+			//for loop to run insert query 5 times
+			for ($id=0; $id <= 5; $id++) 
+			{ 
+				//INSERT query
+				$sql = "INSERT INTO student";
+				$sql .= "(studentid, password, dob, firstname, lastname, house, town, county, country, postcode)";
+				$sql .= "VALUES('{$faker->txtstudentid()}', '{$faker->txtpassword()}', '{$faker->txtdob()}', '{$faker->txtfirstname()}', '{$faker->txtlastname()}', '{$faker->txthouse()}', '{$faker->txttown()}','{$faker->txtcounty()}', '{$faker->txtcountry()}', '{$faker->txtpostcode()}');";
+
+				//Execute query
+				$result = $mysqli->query($conn, $sql);
+			}
+		}
+		
 ?>
--- students.php
+++ students.php
@@ -1,19 +1,49 @@
-<html>
-    <body>
-    <?php
-        include ("dbconnect.inc");
+<?php
 
-        echo "Student Records";
+   include("_includes/config.inc");
+   include("_includes/dbconnect.inc");
+   include("_includes/functions.inc");
 
-        $sql="SELECT*FROM student";
-        $result = $mysqli->query($sql);
+   require_once 'vendor/autoload.php';
 
+   // check logged in
+   if (isset($_SESSION['id'])) 
+   {
 
-        echo "<table align='center' border='1'>";
-        echo "<tr><th width='200' align='left'>ID</th>
-              <th width='200' align='left'>First_Name</th>
-              <th width='200' align='left'>Surname</th>
-              <th width='200' align='left'>Course</th></tr>";
-    ?>
-    </body>
-</html>
+      echo template("templates/partials/header.php");
+      echo template("templates/partials/nav.php");
+
+      // Build SQL statment that selects all students
+      $sql = "SELECT * FROM student;";
+
+      $result = mysqli_query($conn,$sql);
+
+      // prepare page content
+      $data['content'] .= "<table border='1'>";
+      $data['content'] .= "<tr><th colspan='5' align='center'>Modules</th></tr>";
+      $data['content'] .= "<tr><th>Code</th><th>Type</th><th>Level</th></tr>";
+      // Display the modules within the html table
+      while($row = mysqli_fetch_array($result)) 
+      {
+         $data['content'] = "<tr><td> $row[txtstudentid] </td>
+                                 <td> $row[txtpassword] </td>
+                                 <td> $row[txtdob] </td>
+                                 <td> $row[txtfirstname] </td>
+                                 <td> $row[txtlastname] </td>
+                                 <td> $row[txthouse] </td>
+                                 <td> $row[txttown] </td>
+                                 <td> $row[txtcounty] </td>
+                                 <td> $row[txtcountry] </td>
+                                 <td> $row[txtpostcode] </td></tr>";
+      }
+
+      // render the template
+      echo template("templates/default.php", $data);
+
+   } else {
+      header("Location: index.php");
+   }
+
+   echo template("templates/partials/footer.php");
+
+?>
