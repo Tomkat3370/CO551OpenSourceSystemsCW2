6bb3c8ebdf74e59b9ca88f5bb0b7b778b456bd73
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2022-05-29 22:49:37 +0100

    Faker set

--- seedstudents.php
+++ seedstudents.php
@@ -1,28 +1,24 @@
 <?php
 
+	require_once 'vendor/autoload.php';
+
 	include("_includes/config.inc");
 	include("_includes/dbconnect.inc");
 	include("_includes/functions.inc");
-	require_once 'vendor/autoload.php';
+	
 
 	$faker = Faker\Factory::create();
 
 	if (isset($_SESSION['id'])) 
-	{
-
+	{	
 		for ($i=0; $i <= 5; $i++)
 			{ 
-				echo $faker->Studentid();
-				echo $faker->Password();
-				echo $faker->DoB();
-				echo $faker->Firstname();
-				echo $faker->Lastname();
-				echo $faker->House();
-				echo $faker-Town();
-				echo $faker->County();
-				echo $faker->Country();
-				echo $faker->Postcode();
+				$sql = "INSERT INTO student(studentid, firstname, lastname, house, town, county, country, postcode)";
+
+				mysqli_query($conn, $sql);
 			}
+		
+			echo "New students successfully added!";	
 
 	}
 			
