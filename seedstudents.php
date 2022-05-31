b35b0469c12f2255fbb7641ca3aea1f71c2f9cd5
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2022-05-31 19:40:52 +0100

    Update seedstudents.php

--- seedstudents.php
+++ seedstudents.php
@@ -8,28 +8,12 @@
 	if (isset($_SESSION['id']))
 	{	
 					
-		$sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode) VALUES (61983081, NVRq\.$a3K<=Kh~y, 17/02/74, Izotz,Zeng, 12, Linden Avenue, Salford, UK, M6 7HT)";
-
-		mysqli_query($conn, $sql);
-
-		$sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode) VALUES (64433421, a4*vV;6p:]\/qWWt, 07/04/48, Gustave, Colt, 22, Hill Crescent, Dudleston Heath, UK, SY12 9NA";
-
-		mysqli_query($conn, $sql);
-
-		$sql = "INSERT INTO student (studentid, password, dob,  firstname, lastname, house, town, county, country, postcode) VALUES (98850215, dUbSYxv{tk@5vv-5, 22/12/69, Wacława, Kuijpers, Rose Cottage, Heol Y Mynydd, Southerndown, UK, CF32 0SN";
-
-		mysqli_query($conn, $sql);
-
-		$sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode) VALUES (97686560, !ZrtXuDE/4., Rayna, Harrell, 34, Wyresdale Avenue, St Helens, UK, WA10 6QQ";
-
-		mysqli_query($conn, $sql);
-
-		$sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode) VALUES (83060305, x4dV3Q,=n(~;5>zq, 15/01/00, Shammuramat, Nussbaum, 7, Barley Field Court, Halton, UK, LS15 7UE";
+		$sql = "INSERT INTO `student` (`studentid`, `password`, `dob`, `firstname`, `lastname`, `house`, `town`, `county`, `country`, `postcode`) VALUES ('61983081', 'Master', '17/02/74', 'Izotz', 'Zeng', '12', 'Linden', 'Salford', 'UK', 'M6 7HT'), ('64433421', 'a4*vV;6p:]\\/qWWt', '07/04/48', 'Gustave', 'Colt', '22', 'Hill', 'Dudleston', 'UK', 'SY12 9NA'), ('98850215', 'dUbSYxv{tk@5vv-5', '22/12/69', 'Wacława', 'Kuijpers', 'Rose', 'Heol Y Mynydd', 'Southerndown', 'UK', 'CF32 0SN'), ('97686560', '!ZrtXuDE/4.', '22/05/1987', 'Rayna', 'Harrell', '34', 'Wyresdale', 'St Helens', 'UK', 'WA10 6QQ'), ('83060305', 'x4dV3Q,=n(~;5>zq', '15/01/00', 'Shammuramat', 'Nussbaum', '7', 'Barley Field', 'Halton', 'UK', 'LS15 7UE');";
 
 		mysqli_query($conn, $sql);
 			
 		
-			echo "New students successfully added!";	
+		echo "New students successfully added!";	
 	}
 
 ?>
