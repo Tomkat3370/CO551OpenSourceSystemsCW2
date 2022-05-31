82d613f4621267098b8bd8fd43a80f02261894a0
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2022-05-31 19:28:35 +0100

    seed finished

--- seedstudents.php
+++ seedstudents.php
@@ -8,15 +8,15 @@
 	if (isset($_SESSION['id']))
 	{	
 					
-		$sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode) VALUES (61983081, NVRq\.$a3K<=Kh~y, Izotz,Zeng, 12, Linden Avenue, Salford, UK, M6 7HT)";
+		$sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode) VALUES (61983081, NVRq\.$a3K<=Kh~y, 17/02/74, Izotz,Zeng, 12, Linden Avenue, Salford, UK, M6 7HT)";
 
 		mysqli_query($conn, $sql);
 
-		$sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode) VALUES (64433421, a4*vV;6p:]\/qWWt, Gustave, Colt, 22, Hill Crescent, Dudleston Heath, UK, SY12 9NA";
+		$sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode) VALUES (64433421, a4*vV;6p:]\/qWWt, 07/04/48, Gustave, Colt, 22, Hill Crescent, Dudleston Heath, UK, SY12 9NA";
 
 		mysqli_query($conn, $sql);
 
-		$sql = "INSERT INTO student (studentid, password, dob,  firstname, lastname, house, town, county, country, postcode) VALUES (98850215, dUbSYxv{tk@5vv-5, Wacława, Kuijpers, Rose Cottage, Heol Y Mynydd, Southerndown, UK, CF32 0SN";
+		$sql = "INSERT INTO student (studentid, password, dob,  firstname, lastname, house, town, county, country, postcode) VALUES (98850215, dUbSYxv{tk@5vv-5, 22/12/69, Wacława, Kuijpers, Rose Cottage, Heol Y Mynydd, Southerndown, UK, CF32 0SN";
 
 		mysqli_query($conn, $sql);
 
@@ -24,7 +24,7 @@
 
 		mysqli_query($conn, $sql);
 
-		$sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode) VALUES (83060305, x4dV3Q,=n(~;5>zq, Shammuramat, Nussbaum, 7, Barley Field Court, Halton, UK, LS15 7UE";
+		$sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode) VALUES (83060305, x4dV3Q,=n(~;5>zq, 15/01/00, Shammuramat, Nussbaum, 7, Barley Field Court, Halton, UK, LS15 7UE";
 
 		mysqli_query($conn, $sql);
 			
