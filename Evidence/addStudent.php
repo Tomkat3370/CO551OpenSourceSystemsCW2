0fc52b8ed1b75a375c4ab8ffe00c18e5bac86398
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2022-05-31 23:08:44 +0100

    task 4

--- Evidence/addStudent.php
+++ Evidence/addStudent.php
@@ -1,9 +1,5 @@
 <?php
 
-//As the application stands there is no facility to add new students to the student table. Create a new script called addstudent.php that contains a form for entering student details. The details should be submitted to a php script that builds a SQL statement and, when executed, inserts the student details into the student table. Higher marks will be awarded for implementations that include validation and the ability for passwords to be set from this form (and stored securely in the database). 
-
-<?php
-
 include("_includes/config.inc");
 include("_includes/dbconnect.inc");
 include("_includes/functions.inc");
@@ -19,47 +15,60 @@ if (isset($_SESSION['id'])) {
    if (isset($_POST['submit'])) {
 
       // build an sql statment to update the student details
-      $sql = "update student set firstname ='" . $_POST['txtfirstname'] . "',";
+      $sql .= "INSERT INTO student SET studentid ='" . $_POST['txtstudentid']  . "',";
+      $sql .= "password ='" . $_POST['txtpassword']  . "',";
+      $sql = "firstname ='" . $_POST['txtfirstname'] . "',";
       $sql .= "lastname ='" . $_POST['txtlastname']  . "',";
+      $sql .= "dob ='" . $_POST['txtdob']  . "',";
       $sql .= "house ='" . $_POST['txthouse']  . "',";
       $sql .= "town ='" . $_POST['txttown']  . "',";
       $sql .= "county ='" . $_POST['txtcounty']  . "',";
       $sql .= "country ='" . $_POST['txtcountry']  . "',";
       $sql .= "postcode ='" . $_POST['txtpostcode']  . "' ";
       $sql .= "where studentid = '" . $_SESSION['id'] . "';";
+
       $result = mysqli_query($conn,$sql);
 
-      $data['content'] = "<p>Your details have been updated</p>";
+      $data['content'] = "<p>Students details have been added</p>";
+
+      // Normal Password
+    $pass = $_POST['password'];
+ 
+    // Securing password using password_hash
+    $secure_pass = password_hash($pass, PASSWORD_BCRYPT);
+ 
+    $sql = "INSERT INTO login_tb (u_username, u_password)
+    VALUES('$username', '$secure_pass')";
+    $result = mysqli_query($conn, $sql);
 
    }
    else {
-      // Build a SQL statment to return the student record with the id that
-      // matches that of the session variable.
-      $sql = "select * from student where studentid='". $_SESSION['id'] . "';";
-      $result = mysqli_query($conn,$sql);
-      $row = mysqli_fetch_array($result);
-
+      
       // using <<<EOD notation to allow building of a multi-line string
       // see http://stackoverflow.com/questions/6924193/what-is-the-use-of-eod-in-php for info
       // also http://stackoverflow.com/questions/8280360/formatting-an-array-value-inside-a-heredoc
       $data['content'] = <<<EOD
 
-   <h2>My Details</h2>
+   <h2>Add Student Record</h2>
    <form name="frmdetails" action="" method="post">
    First Name :
-   <input name="txtfirstname" type="text" value="{$row['firstname']}" /><br/>
+   <input name="txtfirstname" type="text" value="" /><br/>
    Surname :
-   <input name="txtlastname" type="text"  value="{$row['lastname']}" /><br/>
+   <input name="txtlastname" type="text"  value="" /><br/>
+   Password:
+   <input name="txtpassword" type="text" value="" /><br/>
+   Date of Birth :
+   <input name="txtdob" type="text" value="" /><br/>
    Number and Street :
-   <input name="txthouse" type="text"  value="{$row['house']}" /><br/>
+   <input name="txthouse" type="text"  value="" /><br/>
    Town :
-   <input name="txttown" type="text"  value="{$row['town']}" /><br/>
+   <input name="txttown" type="text"  value="" /><br/>
    County :
-   <input name="txtcounty" type="text"  value="{$row['county']}" /><br/>
+   <input name="txtcounty" type="text"  value="" /><br/>
    Country :
-   <input name="txtcountry" type="text"  value="{$row['country']}" /><br/>
+   <input name="txtcountry" type="text"  value="" /><br/>
    Postcode :
-   <input name="txtpostcode" type="text"  value="{$row['postcode']}" /><br/>
+   <input name="txtpostcode" type="text"  value="" /><br/>
    <input type="submit" value="Save" name="submit"/>
    </form>
 
@@ -75,10 +84,5 @@ EOD;
 }
 
 echo template("templates/partials/footer.php");
-
-?>
-
-
-
 
 ?>
